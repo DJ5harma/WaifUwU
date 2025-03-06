ef4de123d253583e1a91869164b117f3ee4c8a01
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2025-03-07 00:35:32 +0530

    changed the waifu

--- react/src/Components/Waifu.tsx
+++ react/src/Components/Waifu.tsx
@@ -1,8 +1,9 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Loading } from "./Loading";
-import Waifu1 from "../../public/Waifu1";
-import { OrbitControls } from "@react-three/drei";
+// import Waifu1 from "../../public/Waifu1";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
 
 export const Waifu = () => {
 	return (
@@ -14,13 +15,17 @@ export const Waifu = () => {
 				width: "100%",
 				height: "100%",
 			}}
+			shadows
+			camera={{ fov: 15 }}
 		>
 			<Suspense fallback={<Loading loading_message="Loading 3D model..." />}>
 				{(() => {
 					return (
 						<>
-							<Waifu1 scale={0.5} position={[0, -8, 0]} />
 							<OrbitControls />
+
+							<Avatar position={[0, -1.5, 1]} />
+							<Environment preset="sunset" />
 						</>
 					);
 				})()}
